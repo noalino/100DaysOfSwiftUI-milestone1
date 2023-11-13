89a62bb5fa4852c726088e4a513d532b1cfbe50c
Noalino <bgelineau.dev@gmail.com>
2023-11-13 14:02:23 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Noalino on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
